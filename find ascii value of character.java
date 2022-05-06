cf1057dc83a3bc90ecac2ea3d8f50d1bec1dc09a
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-05-06 14:44:12 +0530

    Create find ascii value of character.java

---
+++ find ascii value of character.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        char c;
+        c=s.next().charAt(0);
+        int ascii=c;
+        System.out.println(ascii);
+    }
+}

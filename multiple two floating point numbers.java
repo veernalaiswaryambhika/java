c0c0c7d71696283b310dc4f2ff6463b5c4465346
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-05-06 15:00:44 +0530

    Create multiple two floating point numbers.java

---
+++ multiple two floating point numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Sample{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        float a,b,c;
+        a=s.nextFloat();
+        b=s.nextFloat();
+        c=a*b;
+        System.out.format("%.2f",c);
+    }

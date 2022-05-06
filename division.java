7a2af948df1de2a6c47977843187c646fc61ffb3
veernalaiswaryambhika <84122740+veernalaiswaryambhika@users.noreply.github.com>
2022-05-06 14:41:42 +0530

    Create division.java

---
+++ division.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Sample{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a,b,c;
+        a=s.nextInt();
+        b=s.nextInt();
+        c=a/b;
+        System.out.println(c);
+    }
+}
